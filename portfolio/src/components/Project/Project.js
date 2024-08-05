ca9fd8d8a8768a67096579cce5f094f01169b6ac
Zhnnyyy <perpetuaedemmanuel1225@gmail.com>
2024-08-05 13:58:15 +0800

    update projects

--- portfolio/src/components/Project/Project.js
+++ portfolio/src/components/Project/Project.js
@@ -1,15 +1,32 @@
 import React from "react";
+import "./project.css";
+import Item from "../ProjectItem/Item";
 const bg = require("../../assets/construction.png");
+const ams = require("../../assets/projects/ams.png");
+const twp = require("../../assets/projects/twp.jpg");
 function Project() {
   return (
-    <div
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        display: "flex",
-      }}
-    >
-      <img src={bg} width={500} height={500} />
+    <div className="aboutContainer">
+      <h1 className="aboutTitle">Projects</h1>
+      <div className="about-line"></div>
+
+      <div className="projectContainer">
+        <Item
+          img={ams}
+          title={"Attendance Management System"}
+          tech={"PHP, Java, MySql"}
+          gitLink={"https://github.com/Zhnnyyy/AMS.git"}
+          vidLink={
+            "https://drive.google.com/file/d/11hqffw4qeG4gsRitcGpbszWaOqXjZnkP/preview"
+          }
+        />
+        <Item
+          img={twp}
+          title={"Timekeeping with Payrol System"}
+          tech={"HTML, CSS, NodeJS, ReactNative"}
+          gitLink={"https://github.com/Zhnnyyy/capstone"}
+        />
+      </div>
     </div>
   );
 }
---
+++ portfolio/src/components/ProjectItem/Item.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./item.css";
+import { FaPlay, FaTimes, FaGithub } from "react-icons/fa";
+
+function Item({ gitLink, vidLink, title, tech, img }) {
+  const [show, setShow] = useState(false);
+  const handleClick = () => {
+    if (vidLink) {
+      setShow(true);
+      return;
+    }
+    alert("No available content");
+  };
+  const handleClose = (e) => {
+    e.stopPropagation();
+    setShow(false);
+  };
+  return (
+    <div
+      className="itemcontainer"
+      style={{
+        backgroundImage: `url(${img})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="info">
+        <h3>{title}</h3>
+        <p>{`${tech}`}</p>
+      </div>
+      <div className="overlay"></div>
+      <div className="play1" onClick={handleClick}>
+        <FaPlay className="playicon" />
+      </div>
+      <a href={gitLink} target="_blank" className="play2">
+        <FaGithub className="playicon" />
+      </a>
+      {show && (
+        <div className="iframeContainer">
+          <button className="close-button" onClick={handleClose}>
+            <FaTimes />
+          </button>
+          <iframe
+            src={vidLink}
+            width="60%"
+            height="60%"
+            allow="autoplay"
+          ></iframe>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Item;
